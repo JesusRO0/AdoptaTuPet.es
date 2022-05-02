a0671db5c2d4cdb6dbac01373ba0b9794db966c4
JesusRO0 <6193@iespuertasdelcampo.es>
2022-05-02 12:57:35 +0200

    IMG

--- AdoptaTuPet.es/index.php
+++ AdoptaTuPet.es/index.php
@@ -1,12 +1,50 @@
 <!DOCTYPE html>
-<html lang="en">
+
+<html lang="es-ES">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="author" content="Jesús Roca">
+    <meta name="keywords" content="computers,programming,web design,html,html5">
+    <meta name="descripcion" content="Mi primera página web, para recordar mis conocimientos de HTML">
+    <meta name="lang" content="es-ES">
+    <title>Mis Pinguinos</title>
+    <link rel="stylesheet" href="./pinguinos.css">
+    <link rel="icon" type="image/png" href="./img/logo.png">
+    <script src="https://kit.fontawesome.com/5bf8f62c2a.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans&display=swap" rel="stylesheet"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet"> 
 </head>
 <body>
-    
+    <header>
+        <div class="redes">
+            <h2>Do not forget to follow us in social media!</h2>
+            <div class="iconos">
+                <a href="https://www.instagram.com/?hl=es" target="_blank"><i class="fab fa-instagram-square"></i></a>
+                <a href="https://twitter.com/?lang=es" target="_blank"><i class="fab fa-twitter-square"></i></a>
+                <a href="https://www.youtube.com/" target="_blank"><i class="fab fa-youtube-square"></i></a>
+                <a href="https://www.tiktok.com/foryou?is_copy_url=1&is_from_webapp=v1" target="_blank"><i class="fab fa-tiktok"></i></a>
+            </div>
+        </div>
+        <div class="titulo">
+            <div class="imagenLogo"><img class="imgPerfil" src="./img/logo.png"></div>
+            <h1>AdoptaTuPet.es</h1>
+        </div>
+        <div class="menu">
+
+        </div>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        <div class="pie">IES "Puertas del Campo" - DIW</div>
+    </footer>
 </body>
+
 </html>
