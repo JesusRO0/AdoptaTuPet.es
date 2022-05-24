9df868e44b0f03bdb97055587277897133d089f1
JesusRO0 <6193@iespuertasdelcampo.es>
2022-05-24 18:52:15 +0200

    Comprobacion JS

--- AdoptaTuPet.es/js/index.js
+++ AdoptaTuPet.es/js/index.js
@@ -1,21 +1,15 @@
 var page = 1;
 
-var paR = document.getElementById('errorR');
-var paL = document.getElementById('errorL');
-var butR = document.getElementById('botonRegistro');
-var reg = document.getElementById('openDR');
-var log = document.getElementById('openDL');
-var divR = document.getElementById('divReg');
-var divL = document.getElementById('divL');
-var perfil = document.getElementById("perfil");
-var divPerf = document.getElementById("divPerf");
-var capaOscura = document.getElementById("relleno");
-var html = document.getElementById("html");
-var botonPer = document.getElementById("perfil");
-var miPerf = document.getElementById('miPerf');
-var cerSes = document.getElementById('cerSes');
-var h2Perf = document.getElementById('h2Perf');
-var imgPerf = document.getElementById('imgPerf');
+var rellenadoCamposForm = [false, false, false, false];
+var datosUsuario = [];
+var email = document.getElementById("email").value;
+var errorEmail = document.getElementById("errorEmail");
+var password = document.getElementById("password").value;
+var confirmaPassword = document.getElementById("confirmaPassword").value;
+var errorConfirmaPassword = document.getElementById("errorConfirmaPassword");
+var password2 = document.getElementById("password").value;
+var usuario = document.getElementById("usuario").value;
+var errorPassword = document.getElementById("errorPassword");
+var errorFormulario = document.getElementById("errorFormulario");
 var login = document.getElementById('divL');
 var registro = document.getElementById('divR');
-
