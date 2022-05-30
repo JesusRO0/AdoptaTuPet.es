8d0d790d8845a47e12c1069d009d0789bb5dafa6
JesusRO0 <78034035+JesusRO0@users.noreply.github.com>
2022-05-30 08:15:13 +0200

    Update expresionesRegulares.js

--- AdoptaTuPet.es/js/expresionesRegulares.js
+++ AdoptaTuPet.es/js/expresionesRegulares.js
@@ -1,3 +1,4 @@
+//Simplemente expresiones regulares que se han utilizado para la comprobación de datos.
 
 var expEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 var expPassword =  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#.$($)$-$_])[A-Za-z\d$@$!%*?&#.$($)$-$_]{8,}$/;
