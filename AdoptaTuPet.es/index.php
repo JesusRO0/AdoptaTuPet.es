923261b36e41839956adbbf422e8a27c8efae4c5
JesusRO0 <6193@iespuertasdelcampo.es>
2022-05-30 21:51:09 +0200

    Diseño

--- AdoptaTuPet.es/index.php
+++ AdoptaTuPet.es/index.php
@@ -108,7 +108,7 @@ session_start();
                             </div>
                         </div>";
 
-                        echo "<a class='entraMovil' id='divEntradaMovil'><img src='./views/img/usuario.png'>Entra</a>";
+                        echo "<a href='./views/perfil.php' class='entraMovil' id='divEntradaMovil'><img src='./views/img/usuario.png'>Entra</a>";
                         echo "<script>
                         document.getElementById('divEntradaMovil').style.display = 'none';
                         
